621c163c96cee14a3433ea5939248712f15a0ea4
Austan123 <austan.augustin1@gmail.com>
2022-06-14 22:37:04 +0000

    {traffic}

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from "react";
 //create your first component
 const Home = () => {
-  const [color, setcolor] = useState("");
+  const [color, setColor] = useState("null");
   return (
     <>
       <div className="trafficTop"></div>
       <div className="lightbox">
         <div
-          className={color == "red" ? "redlit" : "red"}
-          onClick={() => setcolor("red")}
+          className={color == "red" ? "red" : "redlite"}
+          onClick={() => setColor("red")}
         ></div>
         <div
-          className={color == "yellow" ? "yellowlit" : "yellow"}
-          onClick={() => setcolor("yellow")}
+          className={color == "yellow" ? "yellow" : "yellowlite"}
+          onClick={() => setColor("yellow")}
         ></div>
         <div
-          className={color == "green" ? "greenlit" : "green"}
-          onClick={() => setcolor("green")}
+          className={color == "green" ? "green" : "greenlite"}
+          onClick={() => setColor("green")}
         ></div>
       </div>
     </>
